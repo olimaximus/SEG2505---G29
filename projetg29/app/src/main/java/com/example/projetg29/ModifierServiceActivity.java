a6c67700590f8c3acb552ce695e49ae96a4d9bf1
Maxime Le Sage <mlesa084@uottawa.ca>
2021-11-28 19:57:02 -0500

    Terminer la fonction modifier services

--- projetg29/app/src/main/java/com/example/projetg29/ModifierServiceActivity.java
+++ projetg29/app/src/main/java/com/example/projetg29/ModifierServiceActivity.java
@@ -1,13 +1,26 @@
 package com.example.projetg29;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class ModifierServiceActivity extends AppCompatActivity {
     String serviceName;
+    Service service;
+    TextView formulaire;
+    TextView document;
+    EditText input;
+    Button btnFormulaire;
+    Button btnDocument;
 
+    @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,12 +29,83 @@ public class ModifierServiceActivity extends AppCompatActivity {
         // Récupérer les infos de l'activité précédente
         Bundle extras = getIntent().getExtras();
         serviceName = extras.getString("Service");
+        service = new Service(serviceName);
 
-        // Définir le text
+
+        // Définir les éléments du layout
         TextView titre = findViewById(R.id.text_service_modifier);
+        formulaire = findViewById(R.id.textDisplayFormulaire);
+        document = findViewById(R.id.textDisplayDocuments);
+        input = findViewById(R.id.editInfoService);
+        btnFormulaire = findViewById(R.id.btn_addFormulaire);
+        btnDocument = findViewById(R.id.btn_addDocument);
+
+
+        // Mettre à jour le texte
+        updateText();
         titre.setText(serviceName);
 
+        // Fonction du bouton formulaire
+        btnFormulaire.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ajouterFormulaire();
+            }
+        });
+
+        // Fonction du bouton formulaire
+        btnDocument.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ajouterDocument();
+            }
+        });
 
 
+
+
+    }
+
+    public void updateText(){
+        // Mettre à jour le texte du formulaire
+        String[] keysFormulaire = service.getInfoKeys();
+        String resultFormulaire = "Formulaire:"+"\n \n";
+        for(int i = 0; i < keysFormulaire.length; i++){
+            resultFormulaire += keysFormulaire[i] + "\n";
+        }
+        formulaire.setText(resultFormulaire);
+
+        // Mettre à jour le texte du document
+        String[] keysDocument = service.getImageKeys();
+        String resultDocument = "Documents:"+"\n \n";
+        for(int i = 0; i < keysDocument.length; i++){
+            resultDocument += keysDocument[i] + "\n";
+        }
+        document.setText(resultDocument);
+
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    public void ajouterFormulaire(){
+        String info = input.getText().toString();
+        if(info.equals("")){
+            Toast.makeText(getApplicationContext(), "Veuillez d'abord entrer une information", Toast.LENGTH_LONG).show();
+        }
+        else {
+            service.setInfo(info, "");
+            updateText();
+        }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    public void ajouterDocument(){
+        String info = input.getText().toString();
+        if(info.equals("")){
+            Toast.makeText(getApplicationContext(), "Veuillez d'abord entrer une information", Toast.LENGTH_LONG).show();
+        }
+        else {
+            service.setImage(info, null);
+            updateText();
+        }
     }
 }
--- projetg29/app/src/main/java/com/example/projetg29/Service.java
+++ projetg29/app/src/main/java/com/example/projetg29/Service.java
@@ -1,30 +1,108 @@
 package com.example.projetg29;
 
+import android.content.Intent;
+import android.os.Build;
+
+import androidx.annotation.RequiresApi;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Set;
+
 public class Service {
-    private Formulaire formulaire;
-    private Document document;
+    private HashMap<String, String> formulaire; // Un formulaire contient des informations auquelles on associe une valeur.
+    // On peut mettre autant d'informations que l'on veut alors on a seulement besoin d'un formulaire
+    // Ex: Prénom, Maxime
+
+    private HashMap<String, Intent> document; // Un document contient des informations auquelles on associe une image sous forme de Intent
+    // Ex: Preuve de domicile, "photo de la preuve de domicile"
+
     private String name;
 
     public Service(String name){
         this.name = name;
-        formulaire = new Formulaire();
-        document = new Document();
+        formulaire = new HashMap<>();
+        document = new HashMap<>();
     }
     public Service(){
         name = "";
-        formulaire = new Formulaire();
-        document = new Document();
-    }
-
-    public Formulaire getFormulaire() {
-        return formulaire;
-    }
-
-    public Document getDocument() {
-        return document;
+        formulaire = new HashMap<>();
+        document = new HashMap<>();
     }
 
     public String getName() {
         return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    // Ajouter/modifier une information du formulaire
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    public void setInfo(String info, String contenu){
+        if(!formulaire.containsKey(info)){
+            formulaire.put(info, contenu);
+        }
+        else{
+            formulaire.replace(info, contenu);
+        }
+    }
+
+    // Obtenir une information du formulaire
+    public String getInfo(String info){
+        return formulaire.get(info);
+    }
+
+    // Obtenir le tableau des clés du formulaire
+    public String[] getInfoKeys(){
+        Set<String> set = formulaire.keySet();
+        String[] result = Arrays.copyOf(set.toArray(), set.toArray().length, String[].class);
+        return result;
+    }
+
+    // Supprimer une information du formulaire
+    public boolean deleteInfo(String info){
+        boolean result = false;
+        if(formulaire.containsKey(info)){
+            formulaire.remove(info);
+            result = true;
+        }
+        return result;
+    }
+
+    // Ajouter/modifier une image du document
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    public void setImage(String name, Intent i) {
+        if(!document.containsKey(name)){
+            document.put(name, i);
+        }
+        else{
+            document.replace(name, i);
+        }
+    }
+
+    // Obtenir une image du document
+    public Intent getImage(String name){
+        return document.get(name);
+    }
+
+    // Obtenir le tableau des clés du document
+    public String[] getImageKeys(){
+        Set<String> set = document.keySet();
+        String[] result = Arrays.copyOf(set.toArray(), set.toArray().length, String[].class);
+
+        return result;
+    }
+
+    // Supprimer une image du document
+    public boolean deleteImage(String name){
+        boolean result = false;
+        if(document.containsKey(name)){
+            document.remove(name);
+            result = true;
+        }
+        return result;
     }
 }
