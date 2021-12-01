0ef1cb1ba6daf8737b8ed08a91b8b815cfbb5628
MichaelMIJ243 <91161686+MichaelMIJ243@users.noreply.github.com>
2021-12-01 15:52:19 -0500

    Update VoirDemandesClients.java
    À finir après avoir écrit les fonctions du client #Michael

--- projetg29/app/src/main/java/com/example/projetg29/VoirDemandesClients.java
+++ projetg29/app/src/main/java/com/example/projetg29/VoirDemandesClients.java
@@ -10,19 +10,42 @@ import android.widget.Spinner;
 import android.widget.Toast;
 
 public class VoirDemandesClients extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
-// Option du spinner
-    // Text dans values -> strings... selection
+
+    private Spinner dropdown_service;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_voir_demandes_clients);
+        //Afficher les demandes des clients
+        //Dropdown pour afficher les demandes des clients
+        dropdown_service = findViewById(R.id.dropDown_demandes);
+        ArrayAdapter<String> adapter2 = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, ####);
+        dropdown_service.setAdapter(adapter2);
+        dropdown_service.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String select = ####[position];
+                int i = select.indexOf(' ');
+                #### = select.substring(0, i);
+                #### = select.substring(i+2, select.length()-1);
+            }
 
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+
+        // Option du spinner
+        //Spinner des confirmation
+        // Text dans values -> strings... selection
         Spinner spinner = findViewById(R.id.planets_spinner);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.Selection, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
         spinner.setOnItemSelectedListener(this);
-
 
     }
 
@@ -36,14 +59,9 @@ public class VoirDemandesClients extends AppCompatActivity implements AdapterVie
     public void onNothingSelected(AdapterView<?> parent) {
 
     }
-
-    //Afficher les demandes des clients
-
-
-
-
-
-
+    
+    //Option du bouton valider
+    //A ecrire apres avoir fait les fonctionnalites du client
 
 
 
