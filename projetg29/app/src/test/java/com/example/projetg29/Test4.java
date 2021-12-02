3f02579bb9848a8c8a9d7572552e2ca125042e0e
adam-kipler <76525313+adam-kipler@users.noreply.github.com>
2021-12-01 22:20:24 -0500

    Create Test4.java

---
+++ projetg29/app/src/test/java/com/example/projetg29/Test4.java
@@ -0,0 +1,18 @@
+package com.example.projetg29;
+
+import org.junit.Test;
+
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
+
+public class Test2 {
+
+    @Test
+    public void getTypeTest(){
+    Compte compte = new ("test","123");
+    Assert.AreEqual(compte.GetType(), typeof(compte));
+    }
+}
